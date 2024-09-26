d739d4408e0b9c5dc744feec6af5cb8b90dbb353
guihocosta <guiihocosta@gmail.com>
2024-09-26 12:36:41 -0300

    Atividades 1 e 2

---
+++ exercicio2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "<h1>URLs Absolutas </h1>",
+        "<a href='https://www.w3.org/'>W3C</a> <br>",
+        "<a href='https://www.google.com/'>Google</a>",
+        "<h1>URLs relativas</h1>",
+        "<a href='urlrelativo.php'>Teste 1</a> <br>",
+        "<a href='urlrelativo2.php'>Teste 2</a>";
+    ?>
+</body>
+</html>
