59ead5d3a23b214178b1106dba42383c2f8a8931
guihocosta <guiihocosta@gmail.com>
2024-10-10 12:12:47 -0300

    Exercicio 3

--- php/exercicio3.php
+++ php/exercicio3.php
@@ -3,40 +3,54 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/exercicio3.css" class="css">
     <title>Resultado Relatório de Clientes</title>
-    <link rel="stylesheet" href="estilo.css">
 </head>
 <body>
 
     <h1>Resultado Relatório de Clientes:</h1>
 
     <?php
-    // Array associativo com 5 clientes
-    $clientes = [
-        ["codigo" => 1, "nome" => "Alberto Silva"],
-        ["codigo" => 2, "nome" => "Bianca Duarte"],
-        ["codigo" => 3, "nome" => "João Almeida"],
-        ["codigo" => 4, "nome" => "Valéria Souza"],
-        ["codigo" => 5, "nome" => "Carlos Pereira"]
-    ];
+    // Criando os arrays de pessoas
+    $pessoa1 = array("codigo" => 1, "nome" => "Alberto Silva");
+    $pessoa2 = array("codigo" => 2, "nome" => "Bianca Duarte");
+    $pessoa3 = array("codigo" => 3, "nome" => "João Gomes");
+    $pessoa4 = array("codigo" => 4, "nome" => "Fernando Pessoa");
+    $pessoa5 = array("codigo" => 5, "nome" => "Ana Clara");
+
+    // Adicionando as pessoas na lista
+    $lista['pessoa'][] = $pessoa1;
+    $lista['pessoa'][] = $pessoa2;
+    $lista['pessoa'][] = $pessoa3;
+    $lista['pessoa'][] = $pessoa4;
+    $lista['pessoa'][] = $pessoa5;
+
+    // Exibindo a tabela
+    foreach($lista as $listapessoas){
     ?>
 
     <table>
-        <thead>
+        <tr>
+            <th>Código</th>
+            <th>Nome</th>
+        </tr>
+        <?php 
+        // Iterando sobre cada pessoa na lista
+        foreach($listapessoas as $valuepessoa){ ?>
             <tr>
-                <th>Código</th>
-                <th>Nome</th>
+                <?php 
+                // Exibindo o código e o nome em colunas
+                foreach($valuepessoa as $value){ 
+                    echo "<td>$value</td>";
+                } 
+                ?>
             </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($clientes as $cliente): ?>
-                <tr>
-                    <td><?php echo $cliente["codigo"]; ?></td>
-                    <td><?php echo $cliente["nome"]; ?></td>
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
+        <?php } ?>
     </table>
+
+    <?php 
+    } 
+    ?>
 
 </body>
 </html>
